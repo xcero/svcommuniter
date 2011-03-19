70bdfaa8e6947e02907af935eb6575d177b9fb5d
jrmoran <jrmoransv@gmail.com>
2011-03-19 01:13:53 -0400

    omnibox added

--- site/javascripts/background.js
+++ site/javascripts/background.js
@@ -1,7 +1,13 @@
 // This will look for localStorage variables, if they are not found the
 // response will contain an undefined value for the localStorage key
-chrome.extension.onRequest.addListener(
-        function(req, sender, sendRes) {
-            sendRes({status: localStorage[req.localvar] || undefined });
-			  }
-);
+chrome.extension.onRequest.addListener(function(req, sender, sendRes) {
+	sendRes({status: localStorage[req.localvar] || undefined });
+});
+
+// omnibox
+
+chrome.omnibox.onInputEntered.addListener(function(str){
+	chrome.tabs.create({
+        "url":"http://www.google.com/search?q=site%3Asvcommunity.org+" + str
+    });
+});
